7f1c1cc1125d4e23adf6c830c756f6775d439696
neitonjc <78852153+neitonjc@users.noreply.github.com>
2022-08-03 11:41:23 -0300

    Update SwaggerConfig.java

--- RabbitMQAPI/src/main/java/com/api/rabbitmq/config/SwaggerConfig.java
+++ RabbitMQAPI/src/main/java/com/api/rabbitmq/config/SwaggerConfig.java
@@ -30,7 +30,7 @@ public class SwaggerConfig {
 	private ApiInfo metaInfo() {
 		ApiInfo apiInfo = new ApiInfo("RabbitMQ API", 
 									  "API para testes de criação de filas e mensagens no RabbitMQ",
-									  "1.7", 
+									  "1.8", 
 									  "Termos de Serviço", 
 									  new Contact("Neiton Junior Carneiro", "https://github.com/neitonjc/", "neitonjc@gmail.com"), null, null, new ArrayList<VendorExtension>()
 						);
