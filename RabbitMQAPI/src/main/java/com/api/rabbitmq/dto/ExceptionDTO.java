bd7a22e3be0b0084c8b2571ba3588cef544ed728
neitonjc <78852153+neitonjc@users.noreply.github.com>
2022-08-04 08:12:03 -0300

    Refactor

--- RabbitMQAPI/src/main/java/com/api/rabbitmq/dto/ExceptionDTO.java
+++ RabbitMQAPI/src/main/java/com/api/rabbitmq/dto/ExceptionDTO.java
@@ -2,13 +2,21 @@ package com.api.rabbitmq.dto;
 
 public class ExceptionDTO {
 	
-	public int httpStatus;
-	public String msg;
+	private int httpStatus;
+	private String msg;
 	
 	public ExceptionDTO(int httpStatus, String msg) {
 		super();
 		this.httpStatus = httpStatus;
 		this.msg = msg;
+	}
+
+	public int getHttpStatus() {
+		return httpStatus;
+	}
+
+	public String getMsg() {
+		return msg;
 	}
 
 }
